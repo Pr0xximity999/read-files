1ddc729af47c43b68fb929a62ba9bd2a6beb3a4e
Thomas Koman <99066789@mydavinci.nl>
2022-04-18 12:11:06 +0200

    made the zenpython even smaller

--- zenOfPython.py
+++ zenOfPython.py
@@ -1,9 +1,6 @@
-import os
+
 from time import sleep
-
-path = os.getcwd()
-content = open(path + '/read-files/README.md', 'r').readlines()
-
-for i in content[1 : -1]:
-    print(i)
-    sleep(1)
+with open('README.md', 'r')as file:
+    for i in file.readlines()[1 : -1]:
+        print(i)
+        sleep(1)
